953e3c7cca328fc6341274ea7dba87ecd27f39d0
heba39920 <hbhsabr47@gmail.com>
2025-06-11 16:18:45 +0300

    remove unused paths

--- src/modules/Shared/Components/AuthLayout/AuthLayout.jsx
+++ src/modules/Shared/Components/AuthLayout/AuthLayout.jsx
@@ -3,11 +3,13 @@ import logo from "../../../../assets/images/4 3.png";
 import "./AuthLayout.css";
 export default function AuthLayout() {
   return (
-    <section className='auth-background row justify-content-center align-items-center'>
+    <section className='overflow-hidden'>
+    <div className='auth-background  row justify-content-center align-items-center'>
     <div className='col-lg-6 col-md-12 col-sm-12'>
       <div className='form-container'>
     <div className='text-center mb-4'> <img className='w-50' src={logo} alt="logo" /></div> 
       <Outlet/>
+    </div>
     </div>
     </div>     
     </section>
--- src/modules/Shared/Components/MasterLayout/MasterLayout.jsx
+++ src/modules/Shared/Components/MasterLayout/MasterLayout.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import NavBar from '../NavBar/Navbar'
-import Header from '../Header/Header'
+
+import NavBar from '../NavBar/NavBar'
+
 import SideBar from '../SideBar/SideBar'
 import { Outlet } from 'react-router-dom'
 
