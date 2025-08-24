88d6d4e492a2197e0448bbf5051089cfa81407f9
heba39920 <hbhsabr47@gmail.com>
2025-08-24 22:19:27 +0300

    deploy

--- src/modules/Shared/Components/MasterLayout/MasterLayout.jsx
+++ src/modules/Shared/Components/MasterLayout/MasterLayout.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import NavBarComponent from '../NavBar/NavBar'
+import NavBarComponent from '../NavBar/Navbar'
 import SideBarComponent from '../SideBar/SideBar'
 import { Outlet } from 'react-router-dom'
 export default function MasterLayout() {
